c975ec22bc8336895fb11b1a8f3b846d81510e39
Éderson Fernandes <edersonluis.rf@gmail.com>
2019-07-23 22:40:42 -0300

    aula 01 primeiros passos com exceções

--- ByteBank/ByteBank/Program.cs
+++ ByteBank/ByteBank/Program.cs
@@ -6,7 +6,7 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(867, 86712540);
+            /*ContaCorrente conta = new ContaCorrente(867, 86712540);
 
             // conta.Agencia = 867;
             conta.Numero = 86712540;
@@ -16,7 +16,44 @@ namespace ByteBank
 
             Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
 
+            Console.ReadLine();*/
+
+            try
+            {
+                Metodo();
+            }
+            catch (DivideByZeroException excecao)
+            {
+                Console.WriteLine("Exceção tratada.");
+                Console.WriteLine(excecao.Message);
+                Console.WriteLine(excecao.StackTrace);
+            }
             Console.ReadLine();
+        }
+
+        public static int Dividir(int numero, int divisor)
+        {
+            ContaCorrente conta = null;
+            Console.WriteLine(conta.Saldo);
+            return numero / divisor;
+        }
+
+        static void Metodo()
+        {
+            try
+            {
+                TestaDivisao(0);
+            }
+            catch (NullReferenceException excecao)
+            {
+                Console.WriteLine(excecao.Message);
+                Console.WriteLine(excecao.StackTrace);
+            }
+        }
+
+        static void TestaDivisao(int divisor)
+        {
+            Dividir(10, divisor);
         }
     }
 }
